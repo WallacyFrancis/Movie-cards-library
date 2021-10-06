ce643c4f46faa52cec43506d98f3bf2c0cca99f2
Wallacy Francis <wallacy_francis@hotmail.com>
2021-10-06 15:13:32 -0300

    Subindo o projeto

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// Wallacy Francis - iniciando o projeto
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import MovieList from './components/MovieList';
+import movies from './data';
+
+function App() {
+  return (
+    <main>
+      <Header />
+      <MovieList movies={ movies } />
+    </main>
+  );
+}
+
+export default App;
